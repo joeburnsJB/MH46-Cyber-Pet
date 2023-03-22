a9e486a89bbd3d28d93dac95fe68e587374ed9d8
deergod <CommonTroll@live.co.uk>
2023-03-22 12:04:07 +0000

    initial commit

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+class Pet {
+    constructor() {
+        this.health = 100;
+        this.hunger = 0;
+        this.happiness = 50;
+    }
+}
+
 // import pet sublclasses
 // main game update function
 // game state object
