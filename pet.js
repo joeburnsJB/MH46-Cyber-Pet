aab73cb457dd74f412b73c95b3134328132a53a3
deergod <CommonTroll@live.co.uk>
2023-03-22 14:43:30 +0000

    updating pet class with new functions and variables to support basic game interaction

--- pet.js
+++ pet.js
@@ -1,12 +1,15 @@
-var petName = document.getElementById("nameInput").value;
-
 class Pet {
     constructor(name) {
         this.name = name;
+        this.health = 100;
         this.hunger = 0;
-        this.happiness = 100;
+        this.happiness = 50;
+        this.bond = 0;
+        this.energy = 100;
+        this.hygiene = 100;
     }
-    eats() {
+
+    eat() {
         if (this.hunger > 40) {
             this.hunger -= 40;
         }
@@ -17,14 +20,28 @@ class Pet {
         // for example --->  petMessage.textContent = `${this.name} ate!)`;
         return this;
     }
+
     play() {
         if (this.happiness < 80) {
-            this.happiness += 20
+            this.happiness += 20;
         }
         else {
-            this.happiness = 100
+            this.happiness = 100;
         }
         // can put line here to change text in the HTML saying that the pet played
         return this;
+    }
+
+    sleep() {
+        // when energy hit 0, goes to sleep
+        // lasts a random amount of time within a range (e.g. between 10-15 mins)
+    }
+
+    defecate() {
+        // random time after eating pet will defecate, will increase dirtiness levels
+    }
+
+    kill() {
+        // when health reaches 0, die
     }
 }
