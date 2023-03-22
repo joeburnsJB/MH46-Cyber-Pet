56bc59f1c2eaab4c9c51c47298712ce553036df0
deergod <CommonTroll@live.co.uk>
2023-03-22 12:53:41 +0000

    adding in placeholder functions and code for the gameState object and the Pet class

--- main.js
+++ main.js
@@ -4,7 +4,18 @@ import "./cat.js";
 import "./dragon.js";
 
 // game state object
+const gameState = {
+    initialise (petType) {
+        // code to set up the game state when a pet is chosen
+    },
 
+    update () {
+        // code to periodically update the game state
+    },
+
+    input (event) {
+        // code to take user input from buttons and apply it to the game state
+    }
+}
 
 // main game update function
-
