37ec038547f9382996ec2c4f7dc346ba93d98cdb
deergod <CommonTroll@live.co.uk>
2023-03-22 13:52:55 +0000

    updating pet class to include a name

--- pet.js
+++ pet.js
@@ -1,6 +1,7 @@
 class Pet {
-    constructor() {
+    constructor(name) {
         // set up basic stats
+        this.name = name;
         this.health = 100;
         this.hunger = 0;
         this.happiness = 50;
